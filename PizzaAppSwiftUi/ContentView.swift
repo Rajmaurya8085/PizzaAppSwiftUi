739b6456a1df688f15e3b82505cff9b407b1f508
Raj Maurya <rajmaurya8085@gmail.com>
2019-11-10 08:25:03 +0530

    Initial Commit

---
+++ PizzaAppSwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaAppSwiftUi
+//
+//  Created by Raj Maurya on 10/11/19.
+//  Copyright © 2019 Raj Maurya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
